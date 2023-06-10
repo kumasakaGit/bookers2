8c2e2fe3ce0c12b26d3fbd63a744099323a7f446
kumasakaGit <git@github.com:kumasakaGit/ruby.git>
2023-06-10 02:18:36 +0000

    [finish partial template]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,24 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @book.user_id = current_user.id
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @books = Book.all
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
